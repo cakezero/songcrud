38332cca92ac629ebde76dee1cc48781f99d25a1
Bright <sareahj.com@gmail.com>
2022-10-05 03:55:24 +0100

    Song app

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.first_name, self.last_name
+
+class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    date_released = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.TextField()
